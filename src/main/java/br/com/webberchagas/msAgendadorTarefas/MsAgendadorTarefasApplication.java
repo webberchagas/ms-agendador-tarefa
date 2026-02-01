a3d6b60cca3416412437b13654889dc57c9dc6a4
webberchagas <webberschagas@gmail.com>
2026-02-01 11:28:05 -0300

    First commit

---
+++ src/main/java/br/com/webberchagas/msAgendadorTarefas/MsAgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.webberchagas.msAgendadorTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAgendadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/webberchagas/msAgendadorTarefas/MsAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.webberchagas.msAgendadorTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
